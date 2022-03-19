170df55363d3831ca5f67c5934f36eaa0a453b0e
nguyentd <dangnguyenful@gmail.com>
2022-03-19 12:50:55 +0700

    React bootstrap + modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import ReactTable from 'react-table-6';
 import 'react-table-6/react-table.css'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 function App() {
   const [employees, setEmployees] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
   const fetchEmployees = () => {
     return new Promise(resolve => {
       fetch('employees.json',{
@@ -28,6 +57,7 @@ function App() {
 
   const updateEmployee = (e, props) => {
     e.preventDefault();
+    setModalShow(true);
     console.log(props.original)
   }
 
@@ -71,6 +101,10 @@ function App() {
 
   return (
     <div className="App">
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
       <ReactTable
         data={employees}
         columns={columns}
