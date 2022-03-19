211f3bb2e49e67b06ca1ddc507ed745f0576fa88
nguyentd <dangnguyenful@gmail.com>
2022-03-19 12:18:13 +0700

    Render react table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import ReactTable from 'react-table-6';
+import 'react-table-6/react-table.css'
 function App() {
+  const [employees, setEmployees] = useState([]);
   const fetchEmployees = () => {
     return new Promise(resolve => {
       fetch('employees.json',{
@@ -24,13 +26,43 @@ function App() {
 
   useEffect(() => {
     getEmployees().then((res) => {
-      console.log(res);
+      if (res && res.employees) {
+        setEmployees(res.employees);
+      }
     })
-  });
+  }, [employees]);
+
+  const columns = [
+    {
+      Header: 'ID',
+      accessor: 'id'
+    },
+    {
+      Header: 'Name',
+      accessor: 'name'
+    },
+    {
+      Header: 'Email',
+      accessor: 'email'
+    },
+    {
+      Header: 'Status',
+      accessor: 'isActive',
+      Cell: props => <span>{props.value ? 'ACTIVE' : 'DEACTIVATED'}</span>
+    },
+    {
+      Header: props => <span>Friend Age</span>
+    }
+  ];
 
   return (
     <div className="App">
-      Get data
+      <ReactTable
+        data={employees}
+        columns={columns}
+        pageSizeOptions={[1, 10, 100, 1000]}
+        defaultPageSize={1000}
+      />
     </div>
   );
 }
