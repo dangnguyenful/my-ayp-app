9d6b16112390a7c9a91e9a73a3c9d67f338d1ab9
nguyentd <dangnguyenful@gmail.com>
2022-03-19 15:37:32 +0700

    Change structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ReactTable from 'react-table-6';
 import 'react-table-6/react-table.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Modal, Button, Form, Row, Col} from 'react-bootstrap';
+import MyVerticallyCenteredModal from './components/modal';
 function App() {
   const [employees, setEmployees] = useState([]);
   const [modalShow, setModalShow] = useState(false);
@@ -27,66 +28,10 @@ function App() {
     const status = allElement.status.checked;
     changeEmployees(id, {name:name, email:email, isActive:status})
   };
-  const MyVerticallyCenteredModal = (props) => {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Form onSubmit={e => handleSubmit(e)}>
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Update employee information
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-              <Form.Group as={Row} className="mb-3" controlId="name">
-                <Form.Label column sm="2">Name</Form.Label>
-                <Col sm="10">
-                  <Form.Control
-                    required
-                    type="text"
-                    placeholder="Name"
-                    defaultValue={informationPopup.name}
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} className="mb-3" controlId="email">
-                <Form.Label column sm="2">Email</Form.Label>
-                <Col sm="10">
-                  <Form.Control
-                    required
-                    type="email"
-                    placeholder="Name"
-                    defaultValue={informationPopup.email}
-                  />
-                </Col>
-              </Form.Group>
-              <Form.Group as={Row} className="mb-3" controlId="status">
-                <Form.Label column sm="2">Status</Form.Label>
-                <Col sm="10">
-                  <Form.Check 
-                    type="switch"
-                    id="status"
-                    defaultChecked={true}
-                  />
-                </Col>
-              </Form.Group>
-              <input type="hidden" id="employeeId" name="employeeId" value={informationPopup.id}></input>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={props.onHide} variant="light">Close</Button>
-            <Button variant="primary" type="submit">Save</Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
-    );
-  };
+  
   const fetchEmployees = () => {
     return new Promise(resolve => {
-      fetch('employees.json',{
+      fetch('dummy-data/employees.json',{
         headers : { 
           'Content-Type': 'application/json',
           'Accept': 'application/json'
@@ -133,7 +78,7 @@ function App() {
       Cell: props => <span>
         [
           {props.original.isActive ? 
-            <a href='javascript(0);' onClick={e=>updateEmployee(e, props)}>Update</a> : <span>Disabled</span>
+            <a href='javascript(0);' onClick={e => updateEmployee(e, props)}>Update</a> : <span>Disabled</span>
           }
         ]
       </span>
@@ -152,6 +97,8 @@ function App() {
     <div className="App">
       <MyVerticallyCenteredModal
         show={modalShow}
+        informationPopup = {informationPopup}
+        handleSubmit = {handleSubmit}
         onHide={() => setModalShow(false)}
       />
       <ReactTable
---
+++ src/components/modal/index.js
@@ -0,0 +1,64 @@
+import 'react-table-6/react-table.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal, Button, Form, Row, Col} from 'react-bootstrap';
+
+function MyVerticallyCenteredModal(props) {
+  const { informationPopup, handleSubmit } = props;
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Form onSubmit={e => handleSubmit(e)}>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Update employee information
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form.Group as={Row} className="mb-3" controlId="name">
+              <Form.Label column sm="2">Name</Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Name"
+                  defaultValue={informationPopup.name}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="email">
+              <Form.Label column sm="2">Email</Form.Label>
+              <Col sm="10">
+                <Form.Control
+                  required
+                  type="email"
+                  placeholder="Name"
+                  defaultValue={informationPopup.email}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="status">
+              <Form.Label column sm="2">Status</Form.Label>
+              <Col sm="10">
+                <Form.Check 
+                  type="switch"
+                  id="status"
+                  defaultChecked={true}
+                />
+              </Col>
+            </Form.Group>
+            <input type="hidden" id="employeeId" name="employeeId" value={informationPopup.id}></input>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide} variant="light">Close</Button>
+          <Button variant="primary" type="submit">Save</Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+    )
+}
+
+export default MyVerticallyCenteredModal;
